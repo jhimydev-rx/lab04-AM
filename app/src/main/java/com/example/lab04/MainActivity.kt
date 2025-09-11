44225aa88f52dd1bd820cc121bcc153bea5f6e68
jhimydev-rx <jhimyreyesdev@gmail.com>
2025-09-11 15:44:16 -0500

    feat(main-activity): anadir icono dentro de la columna

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -5,17 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lab04.ui.theme.Lab04Theme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,6 +49,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 
     MyColumn()
+
 }
 
 @Composable
@@ -51,6 +59,18 @@ fun MyColumn() {
     ) {
         Spacer(modifier = Modifier.size(40.dp))
         Text("Esto es una prueba columna")
+        Spacer(modifier = Modifier.size(16.dp))
+        MyIcon()
+    }
+}
+@Composable
+fun MyIcon(){
+    Row {
+        Icon(
+            imageVector = Icons.Default.AccountCircle,
+            contentDescription = "Incono prueba"
+        )
+        Text("Icono de prueba")
     }
 }
 
